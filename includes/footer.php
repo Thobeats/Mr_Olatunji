ea835d38e53f11a8d2919128d9364d84aea9f293
Thobeats <41216440+Thobeats@users.noreply.github.com>
2021-03-07 09:25:04 +0100

    new

---
+++ includes/footer.php
@@ -0,0 +1,68 @@
+<footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-5 col-lg-4">
+                        <div class="footer-logo text-center">
+                            <img src="<?=$base_url?>/assets/img/logo/Group 1Frame 1.png" alt="Family and Marriage Counselling Affairs">
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-7 col-lg-8 py-3">
+                        <div class="footer-social text-center text-light">
+                             <div class="row justify-content-center">
+                                 <div class="col-lg-3 col-md-4 col-4">
+                                   <p> <i class="bi bi-envelope"></i></p>
+                                   <p class="footer-text">email us</p>
+                                 </div>
+                                 <div class="col-lg-3 col-md-4 col-4">
+                                    <p><i class="bi bi-facebook"></i></p>
+                                    <p class="footer-text">make us your friend</p>
+                                 </div>
+                                 <div class="col-lg-3 col-md-4 col-4">
+                                    <p><i class="bi bi-twitter"></i></p>
+                                    <p class="footer-text">Follow us</p>
+                                 </div>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row copyright bottom-0 positiion-absolute">
+                    <div class="col-12 text-center text-light">
+                        <p>
+                            copyright(c)  <span class="date"></span> Family Affairs. All rights deserved <br>
+                            Designed By Dev Tobi
+                        </p>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
+
+    </main>
+    
+
+    <!-- Bootstrap Script Files -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script>
+        let date = document.querySelector(".date");
+
+        date.innerHTML = new Date().getFullYear();
+
+        let navToggle = document.querySelector(".responsive-menu");
+        let navMenu = document.querySelector(".responsive-menu-list");
+
+        navToggle.addEventListener("click", function(){
+            navMenu.classList.toggle("show-menu");
+            this.classList.toggle("rotate-menu");
+        });
+
+
+        //
+    </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,96 @@
+<?php 
+   $base_url = str_replace("\\", "/", dirname($_SERVER['SCRIPT_NAME']));
+
+    // $port =  $_SERVER['SERVER_PORT'];
+    // $server = $_SERVER['SERVER_NAME'] . ":" . $port;
+
+    // // $base_url = dirname(dirname(__FILE__));
+    // $base_url = $server."/MR%20Olatunji";
+  // echo $base_url;
+
+   
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="<?=$base_url?>/assets/css/style.css">
+
+    <title><?=isset($title) ? $title : 'Family Affairs'; ?></title>
+    <link rel="shortcut icon" href="<?=$base_url?>/assets/img/logo/Frame2.png" />
+</head>
+<body>
+
+    <main class="container-fluid p-0">
+        <header class="py-2">
+            <!-- Header Holding the Logo and the nav bars -->
+<!-- 
+            <div class="row justify-content-end m-0">
+                <div class="col-6 text-capitalize text-center">
+                    <p class="call-assistance text-secondary">need a counsellor? call : +1-862-888-0136</p>
+                </div>
+            </div> -->
+
+            <div class="row">            
+                <div class="col-6 col-md-6 col-sm-6 pl-6">
+                    <!-- The Logo -->
+                    <img src="<?=$base_url?>/assets/img/logo/LogoMakr-4tS4wl.png" class="logo" alt="Family and Marriage Counselling Affairs">
+                </div>
+
+                <div class=" col-6 col-md-6 col-lg-6">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link text-danger" href="<?=$base_url?>/index.php">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-danger" href="<?=$base_url?>/about_me.php">About<span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-danger" href="<?=$base_url?>/pages/blog.php">Gallery <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-danger" href="<?=$base_url?>/pages/contact_me.php">Contact Us <span class="sr-only">(current)</span></a>
+                                </li>
+                            </ul>                            
+                        </div>
+                    </nav>
+                    <div class="responsive-menu text-right pr-4">
+                        <span><i class="bi bi-list"></i></span>
+                    </div>                    
+                </div>
+            </div>   
+        </header>
+
+        <div class="responsive-menu-list bg-white">
+            <ul class="nav flex-column">
+                <li class="nav-item responsive-item">
+                    <a href="<?=$base_url?>/index.php" class="navlink"> Home</a>
+                </li>
+                <li class="nav-item responsive-item">
+                    <a href="<?=$base_url?>/about_me.php" class="navlink">About</a>
+                </li>
+                <li class="nav-item responsive-item">
+                    <a href="<?=$base_url?>/pages/blog.php" class="navlink"> Gallery</a>
+                </li>
+                <li class="nav-item responsive-item">
+                    <a href="<?=$base_url?>/pages/contact_me.php" class="navlink"> Contact us</a>
+                </li>
+            </ul>
+        </div>
+       
+
